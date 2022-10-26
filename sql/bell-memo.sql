4fc71d56e877bac6d97dfd3a73883b06ae0cdfd9
southorange1228 <southorange1228@foxmail.com>
2022-10-26 20:02:14 +0800

    fix: update sql

--- sql/bell-memo.sql
+++ sql/bell-memo.sql
@@ -16,11 +16,11 @@ CREATE TABLE IF NOT EXISTS `memo_data`  (
 CREATE TABLE IF NOT EXISTS `memo_tag` (
     /* 主键ID */
     `id` binary(36) NOT NULL,
-    /* 主键ID */
+    /* 标签名 */
     `name` varchar(255) NOT NULL,
-    /* 主键ID */
+    /* 创建时间 */
     `created` int(11) NOT NULL DEFAULT 0,
-    /* 主键ID */
+    /* 更新时间 */
     `updated` int(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`)
 );
@@ -28,13 +28,13 @@ CREATE TABLE IF NOT EXISTS `memo_tag` (
 CREATE TABLE IF NOT EXISTS `memo_tag_data` (
   /* 主键ID */
   `id` binary(36) NOT NULL, 
-  /* 主键ID */
-  `tag_id` int(10) NOT NULL, 
-  /* 主键ID */
+  /* 标签ID */
+  `tag_id` binary(36) NOT NULL, 
+  /* 记录ID */
   `memo_id` binary(36) NOT NULL, 
-  /* 主键ID */
+  /* 创建时间 */
   `created` int(11) NOT NULL DEFAULT 0, 
-  /* 主键ID */
+  /* 更新时间 */
   `updated` int(11) NOT NULL DEFAULT 0, 
   PRIMARY KEY (`id`)
 );
